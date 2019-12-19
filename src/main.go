8ceecb2799ab035923b0d9bf38fbfe808677bfdb
alex <chan.k.leong@gmail.com>
2019-12-19 23:57:25 +0800

    Added liveness probes

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -21,10 +22,42 @@ func id_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, os.Getenv("NODE_ID"))
 }
 
+func livenessProbe_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "OK")
+}
+
+// Using Global variables here is the easier to do this.
+var livenessProbeCount int = 0
+var livenessProbeCountMax int = 5
+
+func livenessProbe_failAtN_handler(w http.ResponseWriter, r *http.Request) {
+	if livenessProbeCount < livenessProbeCountMax {
+		fmt.Fprintln(w, "OK")
+		livenessProbeCount++
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("Return 500 liveness probe fail simulation")
+	}
+}
+
 func main() {
 	fmt.Println("Server is starting...")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/version", version_handler)
 	http.HandleFunc("/id", id_handler)
+
+	value, exists := os.LookupEnv("LIVEPROBE_FAIL_N")
+	if exists {
+		// Note Atoi() will parse nonsense text values too.
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			livenessProbeCountMax = i
+			fmt.Println("Setting liveness probe to fail at ", value)
+		}
+
+		http.HandleFunc("/liveness", livenessProbe_failAtN_handler)
+	} else {
+		http.HandleFunc("/liveness", livenessProbe_handler)
+	}
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
